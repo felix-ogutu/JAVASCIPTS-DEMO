7cb5ce23b571a5e34c8b7654ecea482dfe3fc4b7
felix <you@example.com>
2021-07-07 12:08:31 +0300

    Files Modified

---
+++ Introduction/Data_types.js
@@ -0,0 +1,24 @@
+//Javascript Data Types
+//String
+//Number
+//Boolean
+//Undefined
+//null
+//symbol
+//object
+
+//Javascript string
+const FirstName = "Felix";
+const LastName = "Omondi";
+console.log(`My first name is ${FirstName} and my last name is ${LastName}`);
+
+//JS Objects
+const student = {
+  firstName: "Felix",
+  lastName: "Ogutu",
+  teleNumber: 02333,
+};
+
+console.log(student);
+console.log(student.teleNumber);
+console.log(student.lastName);
---
+++ Introduction/Examples.js
@@ -0,0 +1,15 @@
+//Programme to find the square root of the number
+//Take the input from the user
+//let number = prompt('Enter the number :');
+//let result = Math.sqrt(number);
+//console.log(`The square root of ${number} is the ${result}`);
+
+//Programme to find the area of the Triangle
+//Take the input from the user
+let height=prompt("Enter the height of the Triangle :")
+let length=prompt("Enter the length of the Triangle :")
+let base=prompt("Enter the base of the Triangle :")
+
+//Calculate the result
+let result=(height*length*base);
+console.log(`The result of area of the Triangle ${result}`);
