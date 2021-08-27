d92d9393c1412282da144fc57c511c5e23a0da8e
felix <you@example.com>
2021-08-27 11:18:44 +0300

    Files Added

--- Introduction/Comparison.js
+++ Introduction/Comparison.js
@@ -27,3 +27,31 @@ if (number1 > number2) {
 
 //If ...else if statement
 //check if the number if the positive ,negative or the zero
+
+//Program to check if the number is the prime number or not
+//take the number from the user
+const marks = parseInt(prompt("Enter a positive number :"));
+
+let isPrime = true;
+
+//Check if the number is equal to 1
+if (marks === 1) {
+  console.log("1 is neither prime nor composite number");
+}
+
+//Check if number is greater than 1
+else if (marks > 1) {
+  //looping through 2 to number -1
+  for (let i = 2; i < marks; i++) {
+    if (marks % i == 0) {
+      isPrime = false;
+      break;
+    }
+  }
+
+  if (isPrime) {
+    console.log(`${marks} is a prime number`);
+  } else {
+    console.log(`${marks} is not a prime number`);
+  }
+}
