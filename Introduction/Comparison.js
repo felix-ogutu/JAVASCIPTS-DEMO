4e2ca89c430e7110a8c35a6b72a6f484db93c4eb
felix <you@example.com>
2021-08-27 10:03:32 +0300

    Added Files

---
+++ Introduction/Comparison.js
@@ -0,0 +1,29 @@
+const a = 3;
+const b = 3;
+console.log((a = b));
+
+//Check if the number is positive number
+const number = prompt("Enter a number :");
+//check if the number is greater than 0
+if (number > 0) {
+  //the body to check the if statement
+  console.log("The number is positive ");
+}
+
+console.log("The if statement is the easy");
+
+//Program to the whether the number is greater than or less than the number
+
+const number1 = 13;
+const number2 = 23;
+
+//IAf statement to check whether the number is the greatest or the the smallest
+
+if (number1 > number2) {
+  console.log("13 is the greatest");
+} else {
+  console.log("23 is the largest ");
+}
+
+//If ...else if statement
+//check if the number if the positive ,negative or the zero
--- Introduction/Data_types.js
+++ Introduction/Data_types.js
@@ -22,3 +22,28 @@ const student = {
 console.log(student);
 console.log(student.teleNumber);
 console.log(student.lastName);
+
+//Js Data Types
+//Numbers
+const marks = 45;
+//Strings
+let subject = "English";
+let subject2 = "French";
+
+//to print the data types on the screen
+console.log(marks);
+console.log(subject);
+console.log(subject2);
+
+//To add another examples
+
+//to create the objects and declare the properties the object
+let students={
+  course_no=314,
+  course_name='sit',
+  student_name="Felix Omondi",
+
+};
+
+console.log(students);
+
